b55b9b027c2258e956912897550759ea78dceac7
Matt Skelley <mattskelley.au@gmail.com>
2023-09-06 21:05:29 +0800

    Fix server.js
    Fix typos.
    Replace local with 127.0.0.1 in mongo connection string.

--- mern-simplesetup/server/server.js
+++ mern-simplesetup/server/server.js
@@ -1,24 +1,30 @@
 import express from 'express';
-import devBundle form './devBundle'
 import path from 'path'
-import template from '../../template'
-import {MongoClient} from 'mongodb'
+import template from './../template'
+import { MongoClient } from 'mongodb'
 
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup'
+// Comment out before building for production
+import devBundle from './devBundle'
+
+const app = express();
+
+// Comment out before building for production
+devBundle.compile(app);
+
+const url = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mernSimpleSetup'
 MongoClient.connect(url, (err, db) => {
+  if (err) throw err;
   console.log("Connected successfully to mongodb server")
   db.close()
 })
 
-const app = express();
 
 app.get('/', (req, res) => {
   res.status(200).send(template())
 })
 
 const CURRENT_WORKING_DIR = process.cwd()
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist') // Serve static files from the dist folder. Make files available on requests from the client
-devBundle.compile(app); // Only run this line in dev. Comment in prod
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist'))) // Serve static files from the dist folder. Make files available on requests from the client
 
 let port = process.env.PORT || 3000;
 app.listen(port, function onStart(err) {
@@ -26,5 +32,4 @@ app.listen(port, function onStart(err) {
     console.log(err)
   }
   console.info('Server started on port %s.', port)
-}
-
+})
