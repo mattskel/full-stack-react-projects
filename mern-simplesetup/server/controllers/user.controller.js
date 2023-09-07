4e3b18d7525fb6124cf4c4b9ce73e23c0417e1ec
Matt Skelley <mattskelley.au@gmail.com>
2023-09-07 19:41:03 +0800

    Reading Updating and Deleting
    The API endpoint to read a single user's data.
    The API endpoint to update a single user.
    The API endpoint to delete a user .

--- mern-simplesetup/server/controllers/user.controller.js
+++ mern-simplesetup/server/controllers/user.controller.js
@@ -42,7 +42,38 @@ const userByID = async (req, res, next, id) => {
     })
   }
 }
-const read = (req, res) => {  }
-const update = (req, res, next) => {  }
-const remove = (req, res, next) => {  }
+const read = (req, res) => {
+  req.profile.hashed_password = undefined
+  req.profile.salt = undefined
+  return res.json(req.profile)
+}
+
+const update = async (req, res) => {
+  try {
+    let user = req.profile
+    user = extend(user, req.body)
+    user.updated = Date.now()
+    await user.save()
+    user.hashed_password = undefined
+    user.salt = undefined
+    res.json(user)
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err)
+    })
+  }
+}
+const remove = async (req, res) => {
+  try {
+    let user = req.profile
+    let deletedUser = await user.remove()
+    deletedUser.hashed_password = undefined
+    deletedUser.salt = undefined
+    res.json(deletedUser)
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err)
+    })
+  }
+}
 export default { create, userByID, read, list, remove, update }
