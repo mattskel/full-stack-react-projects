867ec40a1b3ad99e10e1a0bf4600ace6fb147502
Matt Skelley <mattskelley.au@gmail.com>
2023-09-07 19:19:44 +0800

    Creating a new user
    The API endpoint to create a new user.

--- mern-simplesetup/server/controllers/user.controller.js
+++ mern-simplesetup/server/controllers/user.controller.js
@@ -2,7 +2,19 @@ import User from '../models/user.model'
 import extend from 'lodash/extend'
 import errorHandler from './error.controller'
 
-const create = (req, res, next) => {  }
+const create = async (req, res, next) => {
+  const user = new User(req.body)
+  try {
+    await user.save()
+    return res.status(200).json({
+      message: "Successfully signed up!"
+    })
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err)
+    })
+  }
+}
 const list = (req, res) => {  }
 const userByID = (req, res, next, id) => {  }
 const read = (req, res) => {  }
