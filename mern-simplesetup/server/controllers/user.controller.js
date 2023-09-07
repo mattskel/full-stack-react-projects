5fb4348eab041bdb1fd413a055bb94c88e9958bb
Matt Skelley <mattskelley.au@gmail.com>
2023-09-07 19:28:36 +0800

    Listing all users
    The API endpoint to fetch all the users.

--- mern-simplesetup/server/controllers/user.controller.js
+++ mern-simplesetup/server/controllers/user.controller.js
@@ -15,7 +15,17 @@ const create = async (req, res, next) => {
     })
   }
 }
-const list = (req, res) => {  }
+const list = async (req, res) => {
+  try {
+    let users = await User.find().select('name email updated created')
+    res.json(users)
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err)
+    })
+  }
+}
+
 const userByID = (req, res, next, id) => {  }
 const read = (req, res) => {  }
 const update = (req, res, next) => {  }
