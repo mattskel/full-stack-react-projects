6e9feb309a64b48c9fd84c2de15ad8f9f6aa2cb3
Matt Skelley <mattskelley.au@gmail.com>
2023-09-07 18:43:16 +0800

    Serving an HTML template at a root URL
    Refactor server. Import express app.

--- mern-simplesetup/server/server.js
+++ mern-simplesetup/server/server.js
@@ -1,16 +1,6 @@
 import config from './../config/config'
-import express from 'express';
-import path from 'path'
-import template from './../template'
+import app from './express'
 import mongoose from 'mongoose'
-
-// Comment out before building for production
-import devBundle from './devBundle'
-
-const app = express();
-
-// Comment out before building for production
-devBundle.compile(app);
 
 // Connection URL
 // mongoose.Promise = global.Promise
@@ -18,14 +8,6 @@ mongoose.connect(config.mongoUri)
 mongoose.connection.on('error', (err) => {
   console.error('MongoDB connection error:', err);
 });
-
-
-app.get('/', (req, res) => {
-  res.status(200).send(template())
-})
-
-const CURRENT_WORKING_DIR = process.cwd()
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist'))) // Serve static files from the dist folder. Make files available on requests from the client
 
 app.listen(config.port, (err) => {
   if (err) {
